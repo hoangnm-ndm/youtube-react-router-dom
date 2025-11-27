2bc5db6c95a8682522485c198203eca4393574f7
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-11-27 17:06:19 +0700

    chore: Delete template routes

--- src/layouts/components/FooterClient.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FooterClient = () => {
-  return <div>FooterClient</div>;
-};
-
-export default FooterClient;
--- src/pages/admin/UserManagermentPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserManagermentPage = () => {
-  return <div>UserManagermentPage</div>;
-};
-
-export default UserManagermentPage;
